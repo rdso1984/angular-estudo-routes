e877b90e54838c2ec4b750ea8e6adef14f61aa44
Rafael Oliveira <rdso1984@gmail.com>
2024-02-22 13:35:25 -0300

    Criado primeiras rotas

--- rotas/src/app/app.routes.ts
+++ rotas/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: TitleComponent, pathMatch:'full'},
+    {path:'portfolio', component: CardComponent, pathMatch:'prefix'}
+];
