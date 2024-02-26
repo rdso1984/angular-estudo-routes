0f15e159b2196e055c739be76ba30effbec327a4
Rafael Oliveira <rdso1984@gmail.com>
2024-02-26 13:45:30 -0300

    Incluido rotas

--- rotas/src/app/app.routes.ts
+++ rotas/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 
 export const routes: Routes = [
     {path:'', component: TitleComponent, pathMatch:'full'},
-    {path:'portfolio', component: CardComponent, pathMatch:'prefix'},
+    {path:'portfolio/:id', component: CardComponent, pathMatch:'prefix'},
     {path:'**', redirectTo: ''}
 ];
--- rotas/src/app/pages/portifolio/card/card.component.ts
+++ rotas/src/app/pages/portifolio/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  constructor(private parametrizador: ActivatedRoute){
+    this.parametrizador.params.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
